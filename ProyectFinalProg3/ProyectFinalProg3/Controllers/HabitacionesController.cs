8b3b2350d89d116828ad154ba2c7d6a5e845a965
Carmen Sanchez <carmenmasato@gmail.com>
2020-03-31 19:48:10 -0400

    Controlador de habitaciones

---
+++ ProyectFinalProg3/ProyectFinalProg3/Controllers/HabitacionesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectFinalProg3.Models;
+
+namespace ProyectFinalProg3.Controllers
+{
+    public class HabitacionesController : Controller
+    {
+        private ClinicaContext db = new ClinicaContext();
+
+        // GET: Habitaciones
+        public ActionResult Index()
+        {
+            return View(db.Habitaciones.ToList());
+        }
+
+        // GET: Habitaciones/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Habitaciones habitaciones = db.Habitaciones.Find(id);
+            if (habitaciones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(habitaciones);
+        }
+
+        // GET: Habitaciones/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Habitaciones/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Numero,Tipo,Precio")] Habitaciones habitaciones)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Habitaciones.Add(habitaciones);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(habitaciones);
+        }
+
+        // GET: Habitaciones/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Habitaciones habitaciones = db.Habitaciones.Find(id);
+            if (habitaciones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(habitaciones);
+        }
+
+        // POST: Habitaciones/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Numero,Tipo,Precio")] Habitaciones habitaciones)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(habitaciones).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(habitaciones);
+        }
+
+        // GET: Habitaciones/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Habitaciones habitaciones = db.Habitaciones.Find(id);
+            if (habitaciones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(habitaciones);
+        }
+
+        // POST: Habitaciones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Habitaciones habitaciones = db.Habitaciones.Find(id);
+            db.Habitaciones.Remove(habitaciones);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
