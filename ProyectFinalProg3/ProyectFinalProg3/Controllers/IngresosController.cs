7842d6dea62c9702eb19a2f906dbdef45041083a
Carmen Sanchez <carmenmasato@gmail.com>
2020-03-31 19:47:23 -0400

    Controlador de Ingresos

---
+++ ProyectFinalProg3/ProyectFinalProg3/Controllers/IngresosController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectFinalProg3.Models;
+
+namespace ProyectFinalProg3.Controllers
+{
+    public class IngresosController : Controller
+    {
+        private ClinicaContext db = new ClinicaContext();
+
+        // GET: Ingresos
+        public ActionResult Index()
+        {
+            var ingresos = db.Ingresos.Include(i => i.Habitaciones).Include(i => i.Pacientes);
+            return View(ingresos.ToList());
+        }
+
+        // GET: Ingresos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingresos ingresos = db.Ingresos.Find(id);
+            if (ingresos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ingresos);
+        }
+
+        // GET: Ingresos/Create
+        public ActionResult Create()
+        {
+            ViewBag.Id_Habitacion = new SelectList(db.Habitaciones, "Id", "Tipo");
+            ViewBag.Id_Paciente = new SelectList(db.Pacientes, "Id", "Cedula");
+            return View();
+        }
+
+        // POST: Ingresos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Id_Paciente,Id_Habitacion,Fecha_Inicio")] Ingresos ingresos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Ingresos.Add(ingresos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Id_Habitacion = new SelectList(db.Habitaciones, "Id", "Tipo", ingresos.Id_Habitacion);
+            ViewBag.Id_Paciente = new SelectList(db.Pacientes, "Id", "Cedula", ingresos.Id_Paciente);
+            return View(ingresos);
+        }
+
+        // GET: Ingresos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingresos ingresos = db.Ingresos.Find(id);
+            if (ingresos == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Id_Habitacion = new SelectList(db.Habitaciones, "Id", "Tipo", ingresos.Id_Habitacion);
+            ViewBag.Id_Paciente = new SelectList(db.Pacientes, "Id", "Cedula", ingresos.Id_Paciente);
+            return View(ingresos);
+        }
+
+        // POST: Ingresos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Id_Paciente,Id_Habitacion,Fecha_Inicio")] Ingresos ingresos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ingresos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Id_Habitacion = new SelectList(db.Habitaciones, "Id", "Tipo", ingresos.Id_Habitacion);
+            ViewBag.Id_Paciente = new SelectList(db.Pacientes, "Id", "Cedula", ingresos.Id_Paciente);
+            return View(ingresos);
+        }
+
+        // GET: Ingresos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingresos ingresos = db.Ingresos.Find(id);
+            if (ingresos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ingresos);
+        }
+
+        // POST: Ingresos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ingresos ingresos = db.Ingresos.Find(id);
+            db.Ingresos.Remove(ingresos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
