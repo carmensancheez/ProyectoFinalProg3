adadf1134be68fddde8c7339576d31761ce1a320
Carmen Sanchez <carmenmasato@gmail.com>
2020-03-31 19:49:55 -0400

    controlador de altas

---
+++ ProyectFinalProg3/ProyectFinalProg3/Controllers/AltasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectFinalProg3.Models;
+
+namespace ProyectFinalProg3.Controllers
+{
+    public class AltasController : Controller
+    {
+        private ClinicaContext db = new ClinicaContext();
+
+        // GET: Altas
+        public ActionResult Index()
+        {
+            var altas = db.Altas.Include(a => a.Ingresos);
+            return View(altas.ToList());
+        }
+
+        // GET: Altas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Altas altas = db.Altas.Find(id);
+            if (altas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(altas);
+        }
+
+        // GET: Altas/Create
+        public ActionResult Create()
+        {
+            ViewBag.Id_Ingreso = new SelectList(db.Ingresos, "Id", "Fecha_Inicio");
+            return View();
+        }
+
+        // POST: Altas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Monto,Id_Ingreso,Fecha_Salida,Nombre_Paciente,IumHabitacion,Fecha_Inicio")] Altas altas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Altas.Add(altas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Id_Ingreso = new SelectList(db.Ingresos, "Id", "Fecha_Inicio", altas.Id_Ingreso);
+            return View(altas);
+        }
+
+        // GET: Altas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Altas altas = db.Altas.Find(id);
+            if (altas == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Id_Ingreso = new SelectList(db.Ingresos, "Id", "Fecha_Inicio", altas.Id_Ingreso);
+            return View(altas);
+        }
+
+        // POST: Altas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Monto,Id_Ingreso,Fecha_Salida,Nombre_Paciente,IumHabitacion,Fecha_Inicio")] Altas altas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(altas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Id_Ingreso = new SelectList(db.Ingresos, "Id", "Fecha_Inicio", altas.Id_Ingreso);
+            return View(altas);
+        }
+
+        // GET: Altas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Altas altas = db.Altas.Find(id);
+            if (altas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(altas);
+        }
+
+        // POST: Altas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Altas altas = db.Altas.Find(id);
+            db.Altas.Remove(altas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
