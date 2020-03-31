ec09b4d0ba9f09f6837ad7f5147d0e2dc437901e
Carmen Sanchez <carmenmasato@gmail.com>
2020-03-31 19:46:42 -0400

    Controlador de medicos

---
+++ ProyectFinalProg3/ProyectFinalProg3/Controllers/MedicosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectFinalProg3.Models;
+
+namespace ProyectFinalProg3.Controllers
+{
+    public class MedicosController : Controller
+    {
+        private ClinicaContext db = new ClinicaContext();
+
+        // GET: Medicos
+        public ActionResult Index()
+        {
+            return View(db.Medicos.ToList());
+        }
+
+        // GET: Medicos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medicos medicos = db.Medicos.Find(id);
+            if (medicos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicos);
+        }
+
+        // GET: Medicos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Medicos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nombre,Exequatur,Especialidad")] Medicos medicos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Medicos.Add(medicos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(medicos);
+        }
+
+        // GET: Medicos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medicos medicos = db.Medicos.Find(id);
+            if (medicos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicos);
+        }
+
+        // POST: Medicos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nombre,Exequatur,Especialidad")] Medicos medicos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(medicos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(medicos);
+        }
+
+        // GET: Medicos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medicos medicos = db.Medicos.Find(id);
+            if (medicos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicos);
+        }
+
+        // POST: Medicos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Medicos medicos = db.Medicos.Find(id);
+            db.Medicos.Remove(medicos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
